9ddc0dba20cc5cf3eed5c5b024e593459bf7907c
Joshua <joshua@cnjmail.com>
2026-09-20 10:37:50 -0400

    Add bundled fonts, token CSS, and local font-face loading

--- frontend/src/cards/party-board.ts
+++ frontend/src/cards/party-board.ts
@@ -1,4 +1,5 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, unsafeCSS } from "lit";
+import panelTokens from "../../../custom_components/nestquest/www/nestquest-panel-tokens.css?inline";
 
 import { registerCustomCard, type CardConfig, type HassLike } from "../types";
 
@@ -7,6 +8,8 @@ export class NestQuestPartyBoardCard extends LitElement {
     hass: { attribute: false },
     _config: { state: true },
   };
+
+  static styles = [unsafeCSS(panelTokens)];
 
   hass?: HassLike;
   _config?: CardConfig;
